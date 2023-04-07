87650bfe22db1362ca3bc238894a6baa45001f01
minnnjuuu <kmj020411@naver.com>
2023-04-07 17:32:33 +0900

    입출력과 사칙연산
    2557,1000,1001,10998,1008,10869,10926,18108,10430,2588,11382,10171,10172

---
+++ input_calculate.py
@@ -0,0 +1,60 @@
+#백준 알고리즘 단계별로 풀기
+#파이썬
+#입출력과 사칙연산
+#2557
+print('Hello World!')
+#1000
+a,b=map(int,input().split())
+print(a+b)
+#1001
+a,b=map(int,input().split())
+print(a-b)
+#10998
+a,b=map(int,input().split())
+print(a*b)
+#1008
+a,b=map(int,input().split())
+print(round(a/b,9))
+#10869
+a,b=map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+#10926
+n=input()
+print(n+'??!')
+#18108
+n=int(input())
+print(n-543)
+#10430
+a,b,c=map(int,input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+#2588
+a=int(input())
+b=int(input())
+b_hundred=b//100
+b_ten=b%100//10
+b_one=b%10
+print(b_one*a)
+print(b_ten*a)
+print(b_hundred*a)
+print(a*b)
+#11382
+a,b,c=map(int,input().split())
+print(a+b+c)
+#10171
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+#10172
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
